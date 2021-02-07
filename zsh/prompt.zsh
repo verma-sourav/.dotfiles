106552929bd4dcdf2f4f68df1220bdda5f99e6c5
Camden DeVoogd <camdendevoogd@gmail.com>
2021-02-06 17:08:14 -0800

    Add config file for starship prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,5 @@
 # Using the starship cross-shell prompt: https://starship.rs/
 # Starship is downloaded when the bootstrap script is run
 eval "$(starship init zsh)"
+
+export STARSHIP_CONFIG=$DOTS/zsh/prompt.toml
