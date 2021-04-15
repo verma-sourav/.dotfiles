9901cd33c9e69128d821773b06da6f60c3f2b57f
Camden DeVoogd <camdendevoogd@gmail.com>
2021-04-15 11:41:12 -0700

    Add zsh-autosuggestions

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,2 +1,4 @@
 # https://github.com/zsh-users/zsh-syntax-highlighting
 source $DOTS/submodules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# https://github.com/zsh-users/zsh-autosuggestions
+source $DOTS/submodules/zsh-autosuggestions/zsh-autosuggestions.zsh
