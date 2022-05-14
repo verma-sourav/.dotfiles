a3f3a8a32b3c7e9eb68c1a1d9fce869040a7ffba
Camden DeVoogd <camdendevoogd@gmail.com>
2022-05-14 08:38:50 -0700

    Fix gitconfig not updating after initial install
    Previously, the setup script would not make any updates to the gitconfig
    if the customized file already exist. This file would exist after the
    initial install when the name and email is put in, so any changes to the
    template would not be automatically applied. Instead, the customized
    file would have to be deleted and the dotfiles would have to be
    installed again.
    
    To fix this, I separated the custom part (author, email, etc) and the
    rest of the gitconfig. If the author already exists, we dont need to
    change it. But now, we always concatenate the author information and the
    rest of the config file to update it.

---
+++ git/setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# ----------------------------------------------------------------------------------------------------------------------
+# replace-author.sh
+# Allow the user to update their git author info using sed
+# ----------------------------------------------------------------------------------------------------------------------
+set -euo pipefail
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+AUTHOR_TEMPLATE="$BASEDIR/template-author.gitconfig"
+AUTHOR_CUSTOMIZED="$BASEDIR/.author.gitconfig"
+BASE_TEMPLATE="$BASEDIR/template.gitconfig"
+GITCONFIG="$BASEDIR/.gitconfig"
+
+CREDENTIAL_HELPER="cache"
+NAME=
+EMAIL=
+
+if [[ ! -f "$AUTHOR_CUSTOMIZED" ]]; then
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        CREDENTIAL_HELPER="osxkeychain"
+    fi
+
+    echo "Setting up git author information"
+    read -e -r -p "Please enter the name you would like to use: " NAME
+    read -e -r -p "Please enter the email address you would like to use: " EMAIL
+    sed -e "s/AUTHORNAME/$NAME/g" \
+        -e "s/AUTHOREMAIL/$EMAIL/g"\
+        -e "s/GIT_CREDENTIAL_HELPER/$CREDENTIAL_HELPER/g" \
+        "$AUTHOR_TEMPLATE" > "$AUTHOR_CUSTOMIZED"
+    echo "Updated git author information"
+fi
+
+echo "Updating gitconfig"
+cat "$AUTHOR_CUSTOMIZED" <(echo) "$BASE_TEMPLATE" > "$GITCONFIG"
