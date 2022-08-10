ca6fdf49bb34b9eaf392ea86e833b70d0dafc6e4
Camden DeVoogd <camdendevoogd@gmail.com>
2022-08-10 08:14:50 -0700

    Change colorscheme to onedark

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,5 +1,8 @@
 -- Available themes (if I keep this updated)
 -- kanagawa: https://github.com/rebelot/kanagawa.nvim
 -- tokyonight: https://github.com/folke/tokyonight.nvim
-vim.cmd("colorscheme kanagawa")
+-- gruvbox-material: https://github.com/sainnhe/gruvbox-material
+-- onedark: https://github.com/joshdick/onedark.vim
+-- sonokai: https://github.com/sainnhe/sonokai
+vim.cmd("colorscheme onedark")
 require("lualine").setup()
