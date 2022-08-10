3a6d6357a3232a1c2dd1fda09a58cd0710d94c4f
Camden DeVoogd <camdendevoogd@gmail.com>
2022-08-10 08:07:54 -0700

    Fix bufferline closing incorrect buffer
    With the custom close function I had, if you had one buffer open but
    pressed close on another buffer, it would close the buffer you were in
    instead of the one you tried to close. This is because it was getting
    the current buffer number instead of the one the close was requested on.

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -1,28 +1,24 @@
 -- When closing buffers, I don't want it to switch to the open nvim tree buffer
 -- https://github.com/akinsho/bufferline.nvim/issues/140#issuecomment-888245325
-local function closeBuffer()
-  local treeView = require("nvim-tree.view")
+local function close_buffer(bufnum)
   local bufferline = require("bufferline")
 
-  -- check if NvimTree window was open
-  local explorerWindow = treeView.get_winnr()
-  local wasExplorerOpen = vim.api.nvim_win_is_valid(explorerWindow)
-  local bufferToDelete = vim.api.nvim_get_current_buf()
-
-  if wasExplorerOpen then
-    -- switch to previous buffer (tracked by bufferline)
+  -- Check if NvimTree widnow was open
+  local tree = require("nvim-tree.view").get_winnr()
+  local was_open = vim.api.nvim_win_is_valid(tree)
+  if was_open then
     bufferline.cycle(-1)
   end
 
-  -- delete initially open buffer
-  vim.cmd("bdelete! " .. bufferToDelete)
+  -- Actually delete the buffer
+  vim.cmd("bdelete! " .. bufnum)
 end
 
 require("bufferline").setup({
   options = {
     mode = "buffers",
     diagnostics = "nvim_lsp",
-    close_command = closeBuffer,
+    close_command = close_buffer,
 
     -- https://github.com/akinsho/bufferline.nvim#sidebar-offset-1
     offsets = {
