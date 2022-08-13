b93d4025b3c68a065c4812fa03ee659dbda95bb0
Camden DeVoogd <camdendevoogd@gmail.com>
2022-08-13 13:09:59 -0700

    Fix buffer deleting... again

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -1,17 +1,6 @@
 -- When closing buffers, I don't want it to switch to the open nvim tree buffer
--- https://github.com/akinsho/bufferline.nvim/issues/140#issuecomment-888245325
 local function close_buffer(bufnum)
-  local bufferline = require("bufferline")
-
-  -- Check if NvimTree widnow was open
-  local tree = require("nvim-tree.view").get_winnr()
-  local was_open = vim.api.nvim_win_is_valid(tree)
-  if was_open then
-    bufferline.cycle(-1)
-  end
-
-  -- Actually delete the buffer
-  vim.cmd("bdelete! " .. bufnum)
+  require("bufdelete").bufdelete(bufnum, false)
 end
 
 require("bufferline").setup({
@@ -24,7 +13,6 @@ require("bufferline").setup({
     offsets = {
       {
         filetype = "NvimTree",
-        -- text = "File Explorer",
         highlight = "Directory",
         text_align = "left",
       },
