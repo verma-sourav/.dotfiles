13969617c104150633cdffb4e7ddeff479915b62
Camden DeVoogd <camdendevoogd@gmail.com>
2022-09-23 21:07:49 -0600

    Remove extra neovim themes
    Honestly I don't like those ones that much anyways, they aren't going to
    get any use.

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,8 +1,4 @@
 -- Available themes (if I keep this updated)
 -- kanagawa: https://github.com/rebelot/kanagawa.nvim
--- tokyonight: https://github.com/folke/tokyonight.nvim
--- gruvbox-material: https://github.com/sainnhe/gruvbox-material
--- onedark: https://github.com/joshdick/onedark.vim
--- sonokai: https://github.com/sainnhe/sonokai
 vim.cmd("colorscheme kanagawa")
-require("lualine").setup()
+require("lualine").setup({})
