4648c4979b51830a8475b85e747d2a1f6ad4e827
Camden DeVoogd <camdendevoogd@gmail.com>
2022-10-02 08:05:01 -0700

    Add extra colorscheme options

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,4 +1,10 @@
 -- Available themes (if I keep this updated)
 -- kanagawa: https://github.com/rebelot/kanagawa.nvim
-vim.cmd("colorscheme kanagawa")
+-- nightfox: https://github.com/EdenEast/nightfox.nvim
+-- catppuccin:  https://github.com/catppuccin/nvim
+-- everforest: https://github.com/sainnhe/everforest
+
+vim.g.catppuccin_flavour = "mocha"
+require("catppuccin").setup()
+vim.cmd("colorscheme catppuccin")
 require("lualine").setup({})
