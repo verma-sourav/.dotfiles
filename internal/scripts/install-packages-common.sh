f47e377966abeaa6d29be473a5eed2a96c0bf74e
Camden DeVoogd <camdendevoogd@gmail.com>
2022-10-04 07:21:52 -0700

    Fix double starship install on macOS
    Starship is already installed in the script to install common packages,
    so it is not needed in the macOS homebrew one.
    
    I would prefer to have it installed by homebrew and apt instead of using
    the install script, but the website doesn't mention an apt option.
    Running the installer again should upgrade starship if necessary
    according to their website docs.

--- internal/scripts/install-packages-common.sh
+++ internal/scripts/install-packages-common.sh
@@ -5,9 +5,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$BASEDIR/helpers.sh"
 
-if ! cmd_exists "starship"; then
-    log "Starship prompt is not installed. Installing..."
-    curl -fsSL https://starship.rs/install.sh | sh -s  -- --yes
-fi
+log "Installing/upgrading starship..."
+curl -fsSL https://starship.rs/install.sh | sh -s  -- --yes
 
 go install golang.org/x/tools/cmd/goimports@latest
