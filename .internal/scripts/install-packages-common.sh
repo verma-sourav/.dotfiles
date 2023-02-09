62b87f7cdb77a6603549a604a84ad770121f9b95
Camden DeVoogd <camdendevoogd@gmail.com>
2023-02-08 21:56:40 -0700

    Install starship using brew

--- .internal/scripts/install-packages-common.sh
+++ .internal/scripts/install-packages-common.sh
@@ -5,7 +5,4 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$BASEDIR/helpers.sh"
 
-log "Installing/upgrading starship..."
-curl -fsSL https://starship.rs/install.sh | sh -s  -- --yes
-
 go install golang.org/x/tools/cmd/goimports@latest
