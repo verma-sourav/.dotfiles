f9f9d7a8c1b84fa05729647280a279b2ca721113
Camden DeVoogd <camdendevoogd@gmail.com>
2023-02-18 23:22:50 -0700

    Rework how totmp generates directories
    The old version worked on Linux but not with macOS' version of mktemp.
    Gotta love the GNU vs BSD stuff. This basically does the same thing.

--- fish/functions/totmp.fish
+++ fish/functions/totmp.fish
@@ -1,3 +1,7 @@
 function totmp
-    cd (mktemp --tmpdir --directory totmp.XXXX)
+    set rand_length 5
+    set rand_val (LC_ALL=C tr -dc [:alnum:] </dev/urandom | head -c "$rand_length")
+    set temp_dir "/tmp/totmp.$rand_val"
+    mkdir "$temp_dir"
+    cd "$temp_dir"
 end
