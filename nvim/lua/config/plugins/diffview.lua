b06cb8535d58c58d2cf1732769da2be3ce43568b
Camden DeVoogd <camdendevoogd@gmail.com>
2023-02-25 15:49:26 -0700

    Add the ability to toggle Diffview

--- nvim/lua/config/plugins/diffview.lua
+++ nvim/lua/config/plugins/diffview.lua
@@ -6,4 +6,19 @@ local M = {
    },
 }
 
+-- Diffview is not going to add a toggle function, so this was the approach recommended by the
+-- creator: https://github.com/sindrets/diffview.nvim/issues/188#issuecomment-1217931407
+local function toggle(e)
+   local view = require("diffview.lib").get_current_view()
+   if view then
+      vim.cmd("DiffviewClose")
+   else
+      vim.cmd("DiffviewOpen " .. e.args)
+   end
+end
+
+function M.config()
+   vim.api.nvim_create_user_command("DiffviewToggle", toggle, { nargs = "*" })
+end
+
 return M
