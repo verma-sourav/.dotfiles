76a95c7e49857779a835105fbc67b53c4f956b9b
Camden DeVoogd <camdendevoogd@gmail.com>
2023-03-11 15:19:33 -0700

    Enable spellcheck in gitcommit and markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,9 +1,2 @@
 vim.opt_local.colorcolumn = "100"
-
-local group = vim.api.nvim_create_augroup("markdown", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-   group = group,
-   desc = "Enable spell checking",
-   pattern = "markdown",
-   command = "setlocal spell",
-})
+vim.opt_local.spell = true
