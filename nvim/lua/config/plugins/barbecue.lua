5323a945183f18222d873cbf3e22c64f37f593d6
Camden DeVoogd <camdendevoogd@gmail.com>
2023-03-31 20:04:58 -0600

    Add barbecue to give a vscode-like winbar

---
+++ nvim/lua/config/plugins/barbecue.lua
@@ -0,0 +1,14 @@
+local M = {
+   "utilyre/barbecue.nvim",
+   name = "barbecue",
+   dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+   },
+}
+
+function M.config()
+   require("barbecue").setup()
+end
+
+return M
