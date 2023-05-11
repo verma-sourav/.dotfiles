3c6f7429f916c661909d7fe492e3b9256f9539bd
Camden DeVoogd <camdendevoogd@gmail.com>
2023-05-11 08:55:53 -0600

    Move hex/dec helpers to bin

--- fish/functions/dec-to-hex.fish
+++
@@ -1,4 +0,0 @@
-function dec-to-hex
-    set decimal "$argv[1]"
-    python3 -c "print(hex($decimal))"
-end
--- fish/functions/hex-to-dec.fish
+++
@@ -1,4 +0,0 @@
-function hex-to-dec
-    set hex "$argv[1]"
-    python3 -c "print(int('$hex', 16))"
-end
