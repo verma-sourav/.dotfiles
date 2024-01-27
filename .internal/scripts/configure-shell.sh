e22e2c1f3efd52c34ee88e4397bafa1ce9cec523
Camden DeVoogd <camdendevoogd@gmail.com>
2024-01-26 21:57:38 -0700

    Stop adding fish to shells file
    This was necessary when I was setting fish as the default shell, but
    isn't necessary anymore since I don't set it as the default anymore (it
    causes issues with some systems that expected a posix shell).

--- .internal/scripts/configure-shell.sh
+++ .internal/scripts/configure-shell.sh
@@ -2,16 +2,9 @@
 set -euo pipefail
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-FISH_BIN=$(command -v fish)
-SHELLS_FILE="/etc/shells"
 HUSHFILE="$HOME/.hushlogin"
 
 source "$BASEDIR/helpers.sh"
-
-if ! pattern_exists "$FISH_BIN" "$SHELLS_FILE"; then
-    log "Adding fish ($FISH_BIN) to the list of allowed shells"
-    sudo sh -c "echo $FISH_BIN >> $SHELLS_FILE"
-fi
 
 if ! file_exists "$HUSHFILE"; then
     log "Creating a hushlogin file"
