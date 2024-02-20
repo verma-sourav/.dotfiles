38302d27ec0f08b41ff79f2882debe5ffe9a8825
Camden DeVoogd <camdendevoogd@gmail.com>
2024-02-20 09:34:12 -0700

    Use delta for git diffs
    This also adds the catppuccin theme to bat. Delta uses bat for some
    stuff under the hood.

--- .internal/formulae.Brewfile
+++ .internal/formulae.Brewfile
@@ -10,6 +10,7 @@ brew "python3" unless system "python3 --version > /dev/null"
 brew "wget"
 
 # Dotfiles (including scripts in bin) dependencies
+brew "git-delta"
 brew "fish"
 brew "go"
 brew "gum"
--- .internal/scripts/manage-symlinks.sh
+++ .internal/scripts/manage-symlinks.sh
@@ -47,6 +47,8 @@ create_link() {
 
 main() {
     cleanup_dead_symlinks
+    create_link "$REPO_ROOT/bat" "$HOME/.config/bat"
+    create_link "$REPO_ROOT/espanso/config/default.yml" "$HOME/.config/espanso/config/default.yml"
     create_link "$REPO_ROOT/espanso/config/default.yml" "$HOME/.config/espanso/config/default.yml"
     create_link "$REPO_ROOT/espanso/match/base.yml" "$HOME/.config/espanso/match/base.yml"
     create_link "$REPO_ROOT/fish/config.fish" "$HOME/.config/fish/config.fish"
@@ -54,6 +56,7 @@ main() {
     create_link "$REPO_ROOT/fish/themes" "$HOME/.config/fish/themes"
     create_link "$REPO_ROOT/git/main.gitconfig" "$HOME/.config/git/config"
     create_link "$REPO_ROOT/git/author.gitconfig" "$HOME/.config/git/author.gitconfig"
+    create_link "$REPO_ROOT/git/delta-catppuccin-mocha.gitconfig" "$HOME/.config/git/delta-catppuccin-mocha.gitconfig"
     create_link "$REPO_ROOT/kitty" "$HOME/.config/kitty"
     create_link "$REPO_ROOT/nvim" "$HOME/.config/nvim"
     create_link "$REPO_ROOT/tmux" "$HOME/.config/tmux"
