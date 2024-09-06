0d6bd5bcf65c3a608c9048cfb474d13013f00020
Camden DeVoogd <camdendevoogd@gmail.com>
2024-09-05 22:16:21 -0600

    Allow using 'q' to close oil

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -1,9 +1,24 @@
+local detail = false
 return {
    "stevearc/oil.nvim",
    opts = {
       delete_to_trash = true,
       lsp_file_methods = {
          autosave_changes = true,
+      },
+      keymaps = {
+         ["q"] = "actions.close",
+         ["gd"] = {
+            desc = "Toggle file detail view",
+            callback = function()
+               detail = not detail
+               if detail then
+                  require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+               else
+                  require("oil").set_columns({ "icon" })
+               end
+            end,
+         },
       },
    },
 }
