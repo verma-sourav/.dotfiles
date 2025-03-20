9ec7cd9e3da8fa628b9a828ca2e6db896b2a10d7
Camden DeVoogd <camdendevoogd@gmail.com>
2025-03-19 21:36:58 -0600

    Stop arrow's keymaps from conflicting with mini.jump
    mini.jump configures ; to be the key to repeat the last jump. Instead of
    changing that, I'm moving arrow to use leader+m (like alternative mark).

--- nvim/lua/core/plugins/arrow.lua
+++ nvim/lua/core/plugins/arrow.lua
@@ -3,7 +3,7 @@ return {
    opts = {
       save_key = "git_root",
       show_icons = true,
-      leader_key = ";", -- Key to open bookmarks (shows file list)
-      buffer_leader_key = "<leader>;", -- Key to open buffer-specific bookmarks (shows lines in file)
+      leader_key = "<leader>m", -- Key to open bookmarks (shows file list)
+      buffer_leader_key = "<leader>M", -- Key to open buffer-specific bookmarks (shows lines in file)
    },
 }
